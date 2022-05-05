9fc93a2e94e4021b0dd888cd0ad3d630b63cf6f0
tasniim <tasniim@MacBook-Pro.local>
2022-05-05 13:41:06 +0100

    dynamic img
    Co-authored-by: Wayne Thrower <wayne.thrower@hotmail.co.uk>

--- cloud383/src/App/App.js
+++ cloud383/src/App/App.js
@@ -1,17 +1,22 @@
 import "./App.css"
-import { useEffect } from "react"
+import { useEffect,useState } from "react"
 
 //Components
 import SpecificDate from "../SpecificDate/SpecificDate"
 
 function App() {
+  const [weatherData, setWeatherData] =useState({});
+ 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=895284fb2d2c50a520ea537456963d9c`
 
   async function fetchAPI() {
     const response = await fetch(`${url}`)
     const data = await response.json()
     console.log(data)
+    setWeatherData(data)
   }
+
+
   useEffect(() => {
     fetchAPI()
   }, [])
@@ -19,7 +24,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <SpecificDate />
+      <SpecificDate weatherData={weatherData}/>
     </div>
   )
 }
--- cloud383/src/SpecificDate/SpecificDate.js
+++ cloud383/src/SpecificDate/SpecificDate.js
@@ -1,14 +1,29 @@
-import { useEffect } from "react"
+import { useEffect,useState } from "react"
 import WeatherInfo from "../WeatherInfo/WeatherInfo"
 
-function SpecificDate() {
-  const imageURL =
-    "https://api.unsplash.com/search/photos?query=london&client_id=cVY9k44QlpVuhp5EyI8L7jHK2TppaDn-vwCc2MPkIlg"
+function SpecificDate(data) {
+    const currentBase = data;
+    const [imgData,setImgData]= useState(data)
 
+    function callImg(){
+        if(imgData === null){
+            setImgData('birmingham')
+        }else{
+            const locationEntered = currentBase.weatherData.name;
+            setImgData(locationEntered);
+        }
+         
+    }
+   
+   console.log(imgData)
+  const imageURL =
+    `https://api.unsplash.com/search/photos?query=london&client_id=cVY9k44QlpVuhp5EyI8L7jHK2TppaDn-vwCc2MPkIlg`
+    // console.log(imgData);
   async function fetchImageAPI() {
+    callImg();
     const response = await fetch(`${imageURL}`)
     const data = await response.json()
-    console.log(data)
+    // console.log(data)
   }
   useEffect(() => {
     fetchImageAPI()
