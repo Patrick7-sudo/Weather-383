44e199e5be372b42a26c71be29188e37fc8528d5
waynethrower <wayne.thrower@hotmail.co.uk>
2022-05-05 13:51:12 +0100

    Added props and weatherInfo styling
    Co-authored-by: tasniim bari <tasniim.bari7@gmail.com>

--- cloud383/src/SpecificDate/SpecificDate.js
+++ cloud383/src/SpecificDate/SpecificDate.js
@@ -1,26 +1,30 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react"
 import WeatherInfo from "../WeatherInfo/WeatherInfo"
 
 function SpecificDate(data) {
-    const currentBase = data;
-    const [imgData,setImgData]= useState(data)
+  const currentBase = data
+  const [imgData, setImgData] = useState(data)
 
-    function callImg(){
-        if(imgData === null){
-            setImgData('birmingham')
-        }else{
-            const locationEntered = currentBase.weatherData.name;
-            setImgData(locationEntered);
-        }
-         
+  function callImg() {
+    if (imgData === null) {
+      setImgData("birmingham")
+    } else {
+      const locationEntered = currentBase.weatherData.name
+      setImgData(locationEntered)
     }
-   
-   console.log(imgData)
-  const imageURL =
-    `https://api.unsplash.com/search/photos?query=london&client_id=cVY9k44QlpVuhp5EyI8L7jHK2TppaDn-vwCc2MPkIlg`
-    // console.log(imgData);
+  }
+
+  console.log(imgData)
+  const imageURL = `https://api.unsplash.com/search/photos?query=london&client_id=cVY9k44QlpVuhp5EyI8L7jHK2TppaDn-vwCc2MPkIlg`
+  // console.log(imgData);
+  const cityname = "Birmingham"
+  const temperature = "20"
+  const humidity = "60"
+  const wind = "10"
+  const precipation = "50"
+
   async function fetchImageAPI() {
-    callImg();
+    callImg()
     const response = await fetch(`${imageURL}`)
     const data = await response.json()
     // console.log(data)
@@ -32,7 +36,13 @@ function SpecificDate(data) {
   return (
     <div>
       {/* fetch background image */}
-      <WeatherInfo />
+      <WeatherInfo
+        temperature={temperature}
+        cityname={cityname}
+        humidity={humidity}
+        wind={wind}
+        precipation={precipation}
+      />
     </div>
   )
 }
