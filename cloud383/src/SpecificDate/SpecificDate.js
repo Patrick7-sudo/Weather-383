df76facd59de077488bf48c9dec0cab901029e04
waynethrower <wayne.thrower@hotmail.co.uk>
2022-05-05 12:21:40 +0100

    set up the image fetch
    Co-authored-by: tasniim bari <tasniim.bari7@gmail.com>

--- cloud383/src/SpecificDate/SpecificDate.js
+++ cloud383/src/SpecificDate/SpecificDate.js
@@ -1,11 +1,22 @@
-import { useEffect, useState } from "react"
-import WeatherInfo from "../App/WeatherInfo/WeatherInfo"
+import { useEffect } from "react"
+import WeatherInfo from "../WeatherInfo/WeatherInfo"
 
 function SpecificDate() {
+  const imageURL =
+    "https://api.unsplash.com/search/photos?query=london&client_id=cVY9k44QlpVuhp5EyI8L7jHK2TppaDn-vwCc2MPkIlg"
+
+  async function fetchImageAPI() {
+    const response = await fetch(`${imageURL}`)
+    const data = await response.json()
+    console.log(data)
+  }
+  useEffect(() => {
+    fetchImageAPI()
+  }, [])
+
   return (
     <div>
-      <h1>hello there, Wayne</h1>
-      <p>please create another container for here</p>
+      {/* fetch background image */}
       <WeatherInfo />
     </div>
   )
--- cloud383/src/WeatherInfo/WeatherInfo.js
+++ cloud383/src/WeatherInfo/WeatherInfo.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 
 function WeatherInfo() {
   return <div>{/* container for weather info */}</div>
