01e11493de3b8bd5eb109ab81cf693e300cecf8d
AmandaRichards <Richards.amanda92@gmail.com>
2022-05-05 11:02:00 +0100

    url

--- cloud383/src/App.js
+++ cloud383/src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
+import {useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=895284fb2d2c50a520ea537456963d9c`
+
+
+async function fetchAPI(){
+ const response = await fetch(`${url}`);
+  const data = await response.json();
+console.log(data)
+}
+  useEffect(() => {
+  fetchAPI()
+}, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
