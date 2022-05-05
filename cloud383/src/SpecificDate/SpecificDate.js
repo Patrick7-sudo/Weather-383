974a279552372d14a6d6d9537e29029294fb7599
waynethrower <wayne.thrower@hotmail.co.uk>
2022-05-05 14:46:26 +0100

    added useStates to set the data
    Co-authored-by: tasniim bari <tasniim.bari7@gmail.com>

--- cloud383/src/SpecificDate/SpecificDate.js
+++ cloud383/src/SpecificDate/SpecificDate.js
@@ -5,6 +5,13 @@ function SpecificDate(data) {
   const currentBase = data
   const [imgData, setImgData] = useState(data)
 
+  if (data === null) {
+    const [cityname, setcityname] = useState(data.name)
+    const [temperature, setTemperature] = useState(data.main.temp)
+    const [humidity, setHumidity] = useState(data.main.humidity)
+    const [wind, setWind] = useState(data.wind.speed)
+  }
+
   function callImg() {
     if (imgData === null) {
       setImgData("birmingham")
@@ -17,11 +24,6 @@ function SpecificDate(data) {
   console.log(imgData)
   const imageURL = `https://api.unsplash.com/search/photos?query=london&client_id=cVY9k44QlpVuhp5EyI8L7jHK2TppaDn-vwCc2MPkIlg`
   // console.log(imgData);
-  const cityname = "Birmingham"
-  const temperature = "20"
-  const humidity = "60"
-  const wind = "10"
-  const precipation = "50"
 
   async function fetchImageAPI() {
     callImg()
@@ -41,7 +43,6 @@ function SpecificDate(data) {
         cityname={cityname}
         humidity={humidity}
         wind={wind}
-        precipation={precipation}
       />
     </div>
   )
--- cloud383/src/WeatherInfo/WeatherInfo.js
+++ cloud383/src/WeatherInfo/WeatherInfo.js
@@ -3,15 +3,14 @@
 //styling
 import InfoStyling from "./WeatherInfoCSS.module.css"
 
-function WeatherInfo({ temperature, cityname, humidity, wind, precipation }) {
+function WeatherInfo({ temperature, cityname, humidity, wind }) {
   return (
     <div>
       {/* container for weather info */}
       <p className={InfoStyling.temperature}>{temperature}°C</p>
       <h2 className={InfoStyling.cityname}>{cityname}</h2>
-      <p className={InfoStyling.humidity}>Humiditiy: {humidity}%</p>
+      <p className={InfoStyling.humidity}>Humidity: {humidity}%</p>
       <p className={InfoStyling.windSpeed}>Wind: {wind} MPH</p>
-      <p className={InfoStyling.precipitation}>Precipitation: {precipation}%</p>
     </div>
   )
 }
