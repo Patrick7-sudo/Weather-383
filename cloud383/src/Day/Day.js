b0548e0af9d763a75f106ce366d9e04f88fef9ee
sandraskolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>
2022-05-05 16:24:03 +0100

    Update README file

--- cloud383/src/Day/Day.js
+++ cloud383/src/Day/Day.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import css from './day.module.css'
+import React from "react";
+import css from "./Day.module.css";
 
-export const Day = ({day, temp, weather}) => {
+export const Day = ({ day, temp, weather }) => {
   return (
     <div className={css.container}>
-        <p className={css.day}>{day}</p>
-        <p className={css.temp}>{temp}</p>
-        <p className={css.weather}>{weather}</p>
+      <p className={css.day}>{day}</p>
+      <p className={css.temp}>{temp}</p>
+      <p className={css.weather}>{weather}</p>
     </div>
-  )
-}
+  );
+};
--- cloud383/src/Hours/Hours.js
+++ cloud383/src/Hours/Hours.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import css from "./Hours.module.css"
+import React from "react";
+import css from "./Hours.module.css";
 
-function Hours({hourData}) {
+function Hours({ hourData }) {
   return (
     <div className={css.hourContainer}>
       <img
         href={`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`}
+        alt="Weather Icon"
       />
       <p>{hourData.dt}</p>
     </div>
   );
 }
 
-export default Hours
+export default Hours;
