506f5a92d4afba016453af89ddf5ad8afbc7f5fd
tasniim <tasniim@MacBook-Pro.local>
2022-05-05 11:42:48 +0100

    created SpeficicDate
    Co-authored-by: Wayne Thrower <wayne.thrower@hotmail.co.uk>

---
+++ cloud383/src/SpecificDate/SpecificDate.js
@@ -0,0 +1,12 @@
+import {useEffect,useState} from "react"
+
+function SpecificDate(){
+   return (
+     <div>
+       <h1>hello there, Wayne</h1>
+       <p>please create another container for here</p>
+     </div>
+   );
+}
+
+export default SpecificDate;
